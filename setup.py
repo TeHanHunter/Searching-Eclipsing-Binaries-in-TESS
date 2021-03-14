8b74ad73afcd2860f7c27a7bffc2d41d2da9b546
Te Han <tehanhunter@gmail.com>
2021-03-14 13:32:33 +0800

    setup.py created

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "SEBIT"},
+    packages=setuptools.find_packages(where="SEBIT"),
     python_requires=">=3.6",
 )
